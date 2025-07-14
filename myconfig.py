bce9041ef8f20f307ebe2801b4f87d9251d63321
xxwang1 <45544606+xxwang1@users.noreply.github.com>
2025-07-13 21:46:36 -0700

    Latest version

--- myconfig.py
+++ myconfig.py
@@ -1,8 +1,8 @@
 # Data word size
 num_banks = 1
-word_size = 16
+word_size = 128
 # Number of words in the memory
-num_words = 16
+num_words = 128
 
 num_rw_ports = 0
 num_r_ports = 1
@@ -12,22 +12,29 @@ tech_name = "tsmcN40"
 # Process corners to characterize
 process_corners = [ "TT" ]
 # Voltage corners to characterize
-supply_voltages = [ 3.3 ]
+supply_voltages = [ 1.0 ]
+vddio = 1.2
+gc_type = "Si"
 # Temperature corners to characterize
 temperatures = [ 25 ]
 
-nominal_corner_only = False
-
+nominal_corner_only = True
+delay_chain_stages = 21
+delay_chain_fanout_per_stage = 4
+trim_netlist = True
 route_supplies = True
-check_lvsdrc = True
+level_shifter = True
+# netlist_only = True
+check_lvsdrc = False
+use_pex = False
 inline_lvsdrc = False
 # Output directory for the results
-output_path = "temp"
+output_path = "temp_1p0_iccad"
 # Output file base name
-output_name = "tsmc_gain_cell_16x16"
+output_name = "tsmc_gain_cell_128x128_iccad_vddio_1p2_dcs_21_0417"
 
 # Disable analytical models for full characterization (WARNING: slow!)
-# analytical_delay = False
+analytical_delay = False
 
 # To force this to use magic and netgen for DRC/LVS/PEX
 # Could be calibre for FreePDK45
